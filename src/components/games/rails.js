e29e2bec75812e594e95badcc63d02dd426c25d9
Zhaoheng Hu <490154835@qq.com>
2023-04-25 10:51:22 +0800

    pointer样式

--- src/components/games/rails.js
+++ src/components/games/rails.js
@@ -1,35 +1,44 @@
 import React from "react";
 import levels from "../levels/levels";
-import './rails.css'
+import "./rails.css";
 
 // 构造每个音符块儿
 export function Block({ note }) {
-  
   return (
     <div className="block">
       <span>{note}</span>
     </div>
-  )
+  );
 }
 
-export function Rail({ levelNum }){
-  let notes = levels[levelNum - 1];
-  // 按照notes的长度返回对应若干个Block
+export function Pointer(){
+
   return(
-    <div className="rail">
-      {notes.map((note, index) => 
-        <Block key={index} note={note} />
-      )}
+    <div className="pointer">
     </div>
   )
 }
 
-export default function RailContainer({ className, levelNum }) {
+export function Rail({ levelNum }) {
+  let notes = levels[levelNum - 1];
+  // 按照notes的长度返回对应若干个Block
+  return (
+    <div className="rail">
+      {notes.map((note, index) => (
+        <div className="block-container">
+          <Pointer key={index} />
+          <Block key={index} note={note} />
+        </div>
+      ))}
+    </div>
+  );
+}
 
+export default function RailContainer({ className, levelNum }) {
   return (
     <div className={className}>
       <Rail levelNum={levelNum} />
       {/* <Rail levelNum={levelNum} /> */}
     </div>
-  )
+  );
 }
